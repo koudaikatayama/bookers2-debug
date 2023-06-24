9ae4dc3a288f2c7858e188aa11c2d601ee99eb03
KoudaiKatayama <132341723+koudaikatayama@users.noreply.github.com>
2023-06-24 04:53:45 +0000

    [Stop2]bookers2-debug

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ApplicationRecord
   has_many :user
+  has_one_attached :profile_image
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
 end
