bef20c4f3fd4fe7e08013dca064c2c2b3cd33ca4
KoudaiKatayama <132341723+koudaikatayama@users.noreply.github.com>
2023-07-06 11:20:33 +0000

    [Finish(7a)]bookers2-debug

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,10 @@
 class Book < ApplicationRecord
 
+  belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
-  belongs_to :user
 
   validates :title,presence:true
   validates :body, length:{ minimum: 1, maximum:200 }
