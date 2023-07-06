a92e92109fb59bf6fc627ca01273f8fdff787818
KoudaiKatayama <132341723+koudaikatayama@users.noreply.github.com>
2023-07-06 12:24:25 +0000

    [Finish(8a)]bookers2-debug

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
     get "followers" => "relationships#followers", as: "followers"
   end
   get "/search", to: "searches#search"
+  get 'messages/:id' => 'messages#message', as: 'message'
+  post 'messages' => 'messages#create', as: 'messages'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
